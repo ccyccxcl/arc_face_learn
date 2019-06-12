fe086f26a455f908e7a1eb822cb096cb0755d9fa
jeff.zhu <jeff.zhu@gdsinoyue.com>
2019-06-12 12:36:24 +0800

    添加说明

--- face_recogntion/arc_face/__init__.py
+++ face_recogntion/arc_face/__init__.py
@@ -1,5 +1,9 @@
+# 虹软SDK激活码
 APP_ID = b'8MJMLoy59856HJ4yqcygJQmzopyPsUcY2xSnd2e3hRq3'
 SDK_KEY = b'B7D9jikEEzt5c9DDNtCe2scBxKBqWfQr7h3pWWCfZwbR'
+# 虹软SDK激活库
 ARC_INIT_SO = "./lib/libarcsoft_face.so"
+# 虹软SDK功能函数库
 ARC_ENGINE_SO = "./lib/libarcsoft_face_engine.so"
+# Liunx 系统 lib库
 SYSTEM_LIB = u'/lib/x86_64-linux-gnu/libc.so.6'
