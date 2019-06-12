87d5e2c083c424c3f9112b1a341f336f00b922f6
jeff.zhu <jeff.zhu@gdsinoyue.com>
2019-06-12 12:37:10 +0800

    添加画框以及检测设置SDK使用情况

--- detection.py
+++ detection.py
@@ -7,7 +7,6 @@ import cv2
 
 
 def logging_handle(logging_path):
-    # 保证log定义名字不一样，否则会全部写在一个日志下
     logger_name = os.path.splitext(os.path.split(logging_path)[1])[0]
     # 日志定义
     logger_handle = logging.getLogger(logger_name)
@@ -16,22 +15,29 @@ def logging_handle(logging_path):
     file_handler = logging.FileHandler(logging_path)
     file_handler.setFormatter(formatter)
     logger_handle.addHandler(file_handler)
-
     return logger_handle
 
 
-logger_handle = logging_handle(LOG_PATH)
-df = DetectionFace(DBFACE_PATH, FACE_ACTION, logger_handle)
-
-
 def run(image_path):
+    # 更换设备时，第一次使用需要激活
+    if FACE_ACTION:
+        if os.path.exists(".asf_install.dat") and os.path.exists("freesdk_121232.dat"):
+            os.remove(".asf_install.dat")
+            os.remove("freesdk_121232.dat")
+    logger_handle = logging_handle(LOG_PATH)
+    df = DetectionFace(DBFACE_PATH, FACE_ACTION, logger_handle)
+
     img = cv2.imread(image_path)
     bounding_box_list, predict_name_list, predict_score_list = df.update(img)
     if bounding_box_list:
         for box, name, score in zip(bounding_box_list, predict_name_list, predict_score_list):
-            print(box, name, score)
-            cv2.rectangle(img, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (0, 0, 255),
-                          thickness=2)
+            label = "{}:{}".format(name, str(score))
+            cv2.rectangle(img, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (0, 0, 255), thickness=2)
+            labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+            cv2.rectangle(img, (int(box[0]), int(box[1]) - round(1.5 * labelSize[1])),
+                          (int(box[0]) + round(1.5 * labelSize[0]), int(box[1]) + baseLine),
+                          (255, 255, 255), cv2.FILLED)
+            cv2.putText(img, label, (int(box[0]), int(box[1])), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 0), 1)
         cv2.imwrite("show.jpg", img)
 
 
